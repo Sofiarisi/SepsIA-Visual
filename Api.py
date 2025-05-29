161f28d065fc593d9871bdb4e53fdfc49786f729
Sofiarisi <135023270+Sofiarisi@users.noreply.github.com>
2025-05-29 12:04:19 -0300

    Todo ando con postman

--- Api.py
+++ Api.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import numpy as np
-import random  # Solo usalo si no tenés el modelo todavía
 from modelito import model  # Esto va si ya tenés el modelo cargado
 import uvicorn
+
     
 app = FastAPI()
 
@@ -15,16 +15,11 @@ class SepsisInput(BaseModel):
 
 @app.post("/analizar")
 def predict_probability(datos: SepsisInput):
-    # Simulación del modelo (usá esto si todavía no cargaste uno real)
-    
     instancia = model()
     #pirulo es unna instancia de model que corre la funcion innit (no self) por qeu nose puede usar las funciones de unna clase sin intanciarla
-    # Si ya tenés un modelo entrenado, usá esto:
+   
     valores = [datos.HeartRate, datos.Temperature, datos.RespirationRate]
     prob = instancia.predict_probability(valores)
-    # porcentaje = round(prob * 100, 2)
-
-    # Devolver mensaje como texto
     return {"mensaje": f"La probabilidad de sepsis es del {prob}%."}
 
 
--- modelito.py
+++ modelito.py
@@ -1,4 +1,4 @@
-import random 
+
 
 class model:
     def predict_probability(self,lista):
